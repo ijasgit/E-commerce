1b903442e03fbeb5104448008d55b7d81f006b5b
ijasgit <mohammedijasn3899@gmail.com>
2022-11-21 01:48:01 +0530

    hashrouter add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import Login from "./Login";
 import Newcart from "./NewCart";
@@ -11,13 +12,13 @@ import { store } from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter >
       <Routes>
-        <Route path="/E-commerce" element={<Login />}></Route>
+        <Route path="/" element={<Login />}></Route>
         <Route path="app" element={<App />}></Route>
         <Route path="cart" element={<Newcart />}></Route>
         <Route path="*" element={<h1>not found</h1>}></Route>
       </Routes>
-    </BrowserRouter>
+      </HashRouter>
   </Provider>
 );
